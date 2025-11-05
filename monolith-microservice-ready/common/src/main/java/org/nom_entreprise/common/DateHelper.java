eb9f41214d8a4ca96675c63c4051358b0af199c7
adiarrassouba <adiarrassouba@onpointafrica.com>
2025-11-05 09:37:24 +0000

    first commit

---
+++ monolith-microservice-ready/common/src/main/java/org/nom_entreprise/common/DateHelper.java
@@ -0,0 +1,45 @@
+package org.nom_entreprise.common;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateHelper {
+
+    public static LocalDateTime convertEpochToDate(long epochTime) {
+        return LocalDateTime.ofInstant(Instant.ofEpochSecond(epochTime), ZoneId.systemDefault());
+    }
+
+    public static long convertDateToEpoch(LocalDateTime date) {
+        Instant instant = date.atZone(ZoneId.systemDefault()).toInstant();
+        return instant.getEpochSecond();
+    }
+
+    public static LocalDateTime convertToLocalDateTime(String dateTimeStr, String sourceZone) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTimeStr, formatter);
+        ZonedDateTime sourceZoned = localDateTime.atZone(ZoneId.of(sourceZone));
+        return sourceZoned.toLocalDateTime();
+    }
+
+    public static LocalDateTime now() {
+        return LocalDateTime.now();
+    }
+
+    public static LocalDateTime startOfWeek(LocalDateTime date) {
+        return date.with(java.time.temporal.TemporalAdjusters.previousOrSame(java.time.DayOfWeek.MONDAY));
+    }
+
+    public static LocalDateTime endOfWeek(LocalDateTime date) {
+        return date.with(java.time.temporal.TemporalAdjusters.nextOrSame(java.time.DayOfWeek.SUNDAY));
+    }
+
+    public static LocalDateTime convertDateMtToLocalDateTime(String dateTimeStr) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
+        LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, formatter);
+        return dateTime;
+    }
+
+}
