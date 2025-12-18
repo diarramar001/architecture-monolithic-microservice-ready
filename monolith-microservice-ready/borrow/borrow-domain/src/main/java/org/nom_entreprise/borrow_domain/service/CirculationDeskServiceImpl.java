1e2ee3b87c485f4a4cdf1c280c6b21b171a36a17
adiarrassouba <adiarrassouba@onpointafrica.com>
2025-12-18 19:16:45 +0000

    DDD

--- monolith-microservice-ready/borrow/borrow-domain/src/main/java/org/nom_entreprise/borrow_domain/service/CirculationDeskServiceImpl.java
+++ monolith-microservice-ready/borrow/borrow-domain/src/main/java/org/nom_entreprise/borrow_domain/service/CirculationDeskServiceImpl.java
@@ -15,8 +15,6 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 import org.springframework.beans.factory.InitializingBean;
 
 import java.util.Optional;
